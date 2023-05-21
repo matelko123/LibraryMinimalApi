c32aaa154e2692c61310a2461f734d98146e63ad
matelko <mateusz280798@gmail.com>
2023-05-21 22:32:46 +0200

    Add validation

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.Results;
 using Library.Api.Models;
 using Library.Api.Properties.Data;
 using Library.Api.Services;
@@ -13,18 +15,29 @@ builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
     new SqliteConnectionFactory(connectionString));
 builder.Services.AddSingleton<DatabaseInitializer>();
 builder.Services.AddSingleton<IBookService, BookService>();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapPost("books", async (Book book, IBookService bookService) =>
+app.MapPost("books", async (Book book, 
+    IBookService bookService, IValidator<Book> validator) =>
 {
+    var validationResult = await validator.ValidateAsync(book);
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors);
+    }
+    
     var created = await bookService.CreateAsync(book);
     return created
         ? Results.Created($"/books/{book.Isbn}", book)
-        : Results.BadRequest("A book with that Isbn already exists.");
+        : Results.BadRequest(new List<ValidationFailure>
+        {
+            new ("Isbn", "A book with that Isbn already exists.")
+        });
 });
 
 
